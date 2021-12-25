91165de866a7793b1a4b7dea8b7dc8e52abf652c
ecedogan123 <96653279+ecedogan123@users.noreply.github.com>
2021-12-25 15:27:38 +0300

    Add files via upload

---
+++ 27.11.2021-1.py
@@ -0,0 +1,4 @@
+sayi=5
+while sayi<10:
+    print(sayi:)
+    # while döngüsü kullanarak 1 den 100 e kadar olan sayıları ekrana yazdır
---
+++ sonsuz kere soracak.py
@@ -0,0 +1,8 @@
+# sonsuz kere soracak doğru bilince break yapacak doğru, yanlış if ile sorgulayacam break sonrası while bitiminden devam edecek "sisteme girildi" yazacak
+
+kullanici adi = ogrenci
+if kullanıcı adı = ogrenci
+    print("kullanıcı adı doğru")
+
+elif:
+inp
